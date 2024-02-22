2b3ccf52208d1c2c7d1eaf0cd4b80b86ac72224f
SaleemMalik632 <115183904+SaleemMalik632@users.noreply.github.com>
2024-02-22 17:39:00 +0500

    Add Valid Anagram

---
+++ ValidAnagram.js
@@ -0,0 +1,51 @@
+
+
+var isAnagram = function (s, t) {
+
+    var IsFound = false;
+    let tArray = t.split('');
+    if (s.length != t.length) {
+        return false;
+    }
+    // for (let i = 0; i < s.length; i++) {
+    //     IsFound = false;
+    //     for (let j = 0; j < tArray.length; j++) {
+    //         if (s[i] ==  tArray[j]) {
+    //             tArray[j] = '-';
+    //             IsFound = true; 
+    //             console.log(tArray[j])
+    //             break;
+    //         }
+    //     }
+    //     if (!IsFound) {
+    //         return false;
+    //     }
+    // }
+    let obj = {}
+    let obj1 = {}
+    for (let i = 0; i < s.length; i++) {
+        if (obj[s[i]] === undefined) {
+            obj[s[i]] = 1;
+        } else {
+            obj[s[i]] += 1;
+        }
+    }
+    for (let i = 0; i < t.length; i++) {
+        if (obj1[t[i]] === undefined) {
+            obj1[t[i]] = 1;
+        } else {
+            obj1[t[i]] += 1;
+        }
+    }
+    for (let k = 0; k < s.length; k++) {
+        if (obj[s[k]] != obj1[s[k]]) {
+            return false;
+        }
+    }
+
+    return true;
+};
+
+s = "anagram", t = "nagaram"
+
+console.log(isAnagram(s, t));
